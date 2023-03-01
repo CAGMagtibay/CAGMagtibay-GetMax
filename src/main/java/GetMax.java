ffb1a81576d839c029f089307af5eae2cb79df65
Christopher Magtibay <cagmagtibay@live.com>
2023-03-01 18:18:42 +0000

    Completed Challenge: Get Max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,14 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxVal = arr[0];                        // initialize max value variable to first value of arr
+
+        for (int i = 0; i < arr.length; i++) {      // iterate through each arr element
+            if (arr[i] > maxVal) {                  // if arr element is bigger than current maxVal,
+                maxVal = arr[i];                    // replace maxVal with larger value
+            }
+        }
+
+        return maxVal;
     }
 }
